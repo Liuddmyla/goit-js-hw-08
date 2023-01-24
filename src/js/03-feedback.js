22066c216952f39e5e6062843c32214403a20152
Liuddmyla <112398546+Liuddmyla@users.noreply.github.com>
2023-01-24 16:29:47 +0200

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from "lodash.throttle";
+
+const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('.feedback-form input');
+const textareaEl = document.querySelector('.feedback-form textarea');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+populateInput();
+
+function onFormSubmit(event) { 
+
+    event.preventDefault();
+
+    console.log(formData);
+    event.currentTarget.reset();
+    
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+
+function onFormInput(event) {
+    
+    formData[event.target.name] = event.target.value;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+
+}
+
+function populateInput() {
+    const saveData = localStorage.getItem(STORAGE_KEY);
+    const parseSaveData = JSON.parse(saveData);
+
+    if (parseSaveData) {        
+        inputEl.value = parseSaveData.email;
+        textareaEl.value = parseSaveData.message;       
+    };
+};
+
+
 
